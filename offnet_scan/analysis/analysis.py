900cbfe7231ea5e79dac0efdcd36ec4f5ae7f746
Leonard von Lojewski <dev@vonlojewski.me>
2022-09-28 14:45:40 +0200

    feat(offnet): add new plots
    Former-commit-id: 83220d94b9d02b8d945a8674f67fcaf26fdd6082

--- offnet_scan/analysis/analysis.py
+++ offnet_scan/analysis/analysis.py
@@ -1,12 +1,17 @@
+import json
+
 import pandas as pd
+
 pd.options.display.max_rows = 200
-import numpy as np
-import matplotlib.pyplot as plt
-import gc
 import functools
-import sys
-import subprocess
+import gc
 import hashlib
+import subprocess
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def map(row):
     ip = row["ip"]
@@ -34,20 +39,20 @@ def load_data(file_name):
     }
     df = pd.read_csv(file_name, **load_config)
     df = df[pd.notna(df["cert"])]
-    mapped = df.apply(map, axis=1, result_type='expand')
-    return mapped
+    # mapped = df.apply(map, axis=1, result_type='expand')
+    return df
 
-dfs = []
-for i in range(0, 10):
-    file = './2022-08-21/scans_ready/output_global_0' + str(i) +'.txt'
-    df = load_data(file)
-    dfs.append(df)
-for i in range(10, 20):
-    file = './2022-08-21/scans_ready/output_global_' + str(i) +'.txt'
-    df = load_data(file)
-    dfs.append(df)
-df = pd.concat(dfs)
-per_asn = df.groupby(["asn"], sort=False).aggregate(count).sort_values(by=['ip'], ascending=False)
-print(per_asn.to_string())
-per_cert = df.groupby(["cert"], sort=False).size().reset_index().sort_values(by=[0], ascending=False)
-print(per_cert.head(1000))
+file = './2022-08-21/scans_ready/output_global.txt'
+df = load_data(file)
+print(df.describe())
+print(df['status'].unique())
+print(df['status'].value_counts())
+print(df[df['status'] == 'success-meta-header'])
+for cert in df[df['status'] == 'success-meta-header']['cert']:
+    parsed = json.loads(cert)
+    print(parsed["data"]["http"]["result"]["response"]["request"][
+            "tls_log"]["handshake_log"]["server_certificates"]["certificate"]["parsed"]['subject']["common_name"])
+# per_asn = df.groupby(["asn"], sort=False).aggregate(count).sort_values(by=['ip'], ascending=False)
+# print(per_asn.to_string())
+# per_cert = df.groupby(["cert"], sort=False).size().reset_index().sort_values(by=[0], ascending=False)
+# print(per_cert.head(1000))
--- offnet_scan/analysis/plot_per_as.py
+++ offnet_scan/analysis/plot_per_as.py
@@ -1,7 +1,8 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import gc
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 
 def load_data(file_name):
@@ -12,24 +13,26 @@ def load_data(file_name):
     }
     return pd.read_csv(file_name, **load_config)
 
-ases = load_data('hosts_per_AS.csv').filter(items=['asn', 'hosts'])
-ases = ases[ases['asn']!='AS32934']
+ases = load_data('hosts_per_AS.csv')
+ases = ases[ases['asn']!='AS32934'].filter(items=['hosts', 'asn'])
 
-ax = ases.plot.line(x='asn')
-ax.set_ylabel("Hosts")
-fig = ax.get_figure()
+# ax = plt.hist(ases['hosts'], cumulative=True, label='CDF',
+#          histtype='step', alpha=0.8, bins=1000)
+ases['hosts'].plot.line(legend=False, xlabel='Top AS by number of hosts', ylabel='# of hosts', grid=True)
+# ax.set_ylabel("# of hosts")
+# fig = ax.get_figure()
 plt.savefig("plots/hosts_per_AS.png")
 
 ax = ases.head(100).plot.bar(x='asn',y='hosts' )
-ax.set_ylabel("Hosts")
+ax.set_ylabel("# of hosts")
 ax.set_xlabel("ASes")
-ax.tick_params(axis='x', labelsize=0)
+ax.tick_params(axis='x')
 fig = ax.get_figure()
 plt.savefig("plots/host_distribution_top_100.png")
 
 ax = ases[ases['hosts']>=100].plot.bar(x='asn',y='hosts',rot=35)
-ax.set_ylabel("Hosts")
-ax.tick_params(axis='x', labelsize=10)
+ax.set_ylabel("# of hosts")
+ax.tick_params(axis='x')
 fig = ax.get_figure()
 plt.savefig("plots/hosts_per_AS_min_100.png")
 
